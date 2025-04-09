0aee6a1717dd6bdb351907383c6d551bac1d375b
tongvantruong <tong.tvt.truong@gmail.com>
2025-04-09 22:46:05 +0700

    setup vite, vue, typescript

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app">
+        <h1>Hello from Vue 3 + TypeScript 🎉</h1>
+    </div>
+</template>
+  
+<script setup lang="ts">
+
+</script>
+  
+<style scoped>
+.app {
+    text-align: center;
+}
+</style>
+  
